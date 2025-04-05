e98b815a582585bba79b7b79a1850bb04e62d59d
Марина <maryna.yatsenko@ukma.edu.ua>
2025-04-06 01:35:53 +0300

    Initial commit

---
+++ src/main/java/org/example/chessearch_back/ChessearchBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.chessearch_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessearchBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessearchBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/chessearch_back/ChessearchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chessearch_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessearchBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
